1f2340aae1030740758a6aa4b4f372260f265b52
Alok Hom <alok.hom@gmail.com>
2025-06-07 11:53:17 +0200

    add kube config

--- terraform-files/provider.tf
+++ terraform-files/provider.tf
@@ -1,10 +1,24 @@
+locals {
+    kube_config = yamldecode(file("~/.kube/gke_config"))
+}
+
 provider "kubernetes" {
-  config_path = file(var.credentials_file)
+    load_config_file = false
+
+    host = local.kube_config.clusters[1].cluster.server
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[1].cluster.certificate-authority-data)
+    client_certificate = base64decode(local.kube_config.users[1].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[1].user.client-key-data)
 }
 
 provider "helm" {
   kubernetes {
-    config_path = file(var.credentials_file)
+    load_config_file = false
+
+    host = local.kube_config.clusters[1].cluster.server
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[1].cluster.certificate-authority-data)
+    client_certificate = base64decode(local.kube_config.users[1].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[1].user.client-key-data)
   }
 }
 
